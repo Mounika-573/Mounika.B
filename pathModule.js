604181e299932d5cc769bfa68b10d9160e1a321a
Mounika-573 <mounika572003@gmail.com>
2024-07-06 13:34:44 +0530

    Update pathModule.js

--- pathModule.js
+++ pathModule.js
@@ -2,14 +2,14 @@ const {fileURLToPath}=require('url');
 const path=require('path');
 const {readFile,writeFile}=require('fs');
 
-const _filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(_filename);
-console.log(__dirname);
-readFile(path.join(__dirname),'utf-8',(err,data)=>{
+const _filename=_filename;
+const __dirname1=path.dirname(_filename);
+console.log(__dirname1);
+readFile(path.join(__dirname1,'customer.json'),'utf-8',(err,data)=>{
     if(err)throw err;
     let customers=JSON.parse(data);
-    writeFile(path.join(__dirname,'util','customers.join'),JSON.stringify(customers),'utf-8',(err,data)=>{
+    writeFile(path.join(__dirname1,'customer.json'),JSON.stringify(customers),'utf-8',(err,data)=>{
         if(err)throw err;
-        console.log('Data written Successfully');
+        console.log("Data written Successfully")
     })
 });
